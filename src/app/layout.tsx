d3034d7756d427b4acc1dd40e7fbb6d7dfa23455
JhimmieC137 <toluwalope.david@gmail.com>
2025-01-30 11:03:43 +0100

    loaded up svgs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Aparte Admin",
-  description: "The official admin console of Aparte",
+  description: "The official admin console of Aparte NG",
   icons: ["/svg/logo.svg"],
 };
 
