2c13a36e4f975248b9b893436dd472f4a38492f4
Pamilerin Idowu <pamilerinid@gmail.com>
2026-02-01 21:48:25 +0100

    feat: Integrate Sentry for error monitoring and global error handling.

--- next.config.ts
+++ next.config.ts
@@ -28,15 +28,17 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: "aparte",
   project: "admin-dashboard",
-}, {
+
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
+  // Automatically delete source maps after uploading to Sentry
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
 
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
   tunnelRoute: "/monitoring",
